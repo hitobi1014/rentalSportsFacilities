087ec7b0b940aa938e6c002918e8403e4da16854
minjun <hitobi1014@gmail.com>
2023-10-20 12:31:19 +0900

    init

---
+++ src/main/java/com/greg/sfms/SfmsApplication.java
@@ -0,0 +1,13 @@
+package com.greg.sfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfmsApplication.class, args);
+    }
+
+}
